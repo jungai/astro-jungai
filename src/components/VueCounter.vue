95c830bb9219df71f7c3ee269dbaacd133958731
jungai <juworaphol125@gmail.com>
2021-11-25 00:41:05 +0700

    script setup

--- src/components/VueCounter.vue
+++ src/components/VueCounter.vue
@@ -6,20 +6,10 @@
   </div>
 </template>
 
-<script lang="ts">
-import { ref } from "vue";
+<script lang="ts" setup>
+import { ref } from 'vue';
 
-export default {
-  setup() {
-    const count = ref(0);
-    const add = () => (count.value = count.value + 1);
-    const subtract = () => (count.value = count.value - 1);
-
-    return {
-      count,
-      add,
-      subtract,
-    };
-  },
-};
+const count = ref(1);
+const add = () => (count.value = count.value + 1);
+const subtract = () => (count.value = count.value - 1);
 </script>
